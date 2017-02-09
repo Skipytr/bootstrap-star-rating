b8e89a5991e6fa14f46d30402a9f5343a996247f
Skipytr <hburaktasyurek@gmail.com>
2017-02-10 00:16:45 +0300

    Turkish Translate

---
+++ js/locales/tr.js
@@ -0,0 +1,31 @@
+/*!
+ * Star Rating Turkish Translations
+ *
+ * This file must be loaded after 'star-rating.js'. Patterns in braces '{}', or
+ * any HTML markup tags in the messages must not be converted or translated.
+ *
+ * NOTE: this file must be saved in UTF-8 encoding.
+ *
+ * @see http://github.com/kartik-v/bootstrap-star-rating
+ * @author Kartik Visweswaran <kartikv2@gmail.com>
+ */
+(function ($) {
+    "use strict";
+    $.fn.ratingLocales['tr'] = {
+        defaultCaption: '{rating} Yıldız',
+        starCaptions: {
+            0.5: '0.5 Yıldız',
+            1: '1 Yıldız',
+            1.5: '1.5 Yıldız',
+            2: '2 Yıldız',
+            2.5: '2.5 Yıldız',
+            3: '3 Yıldız',
+            3.5: '3.5 Yıldız',
+            4: '4 Yıldız',
+            4.5: '4.5 Yıldız',
+            5: '5 Yıldız'
+        },
+        clearButtonTitle: 'Temizle',
+        clearCaption: 'Oy verilmedi'
+    };
+})(window.jQuery);
